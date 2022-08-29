d6b9c3d474c9504a7b900870e5e30f0162cf1172
devKimHD <khd30425@gmail.com>
2022-08-29 15:36:43 +0900

    jsp 푸시테스트

--- src/main/java/solvice/khd/jsptest/HelloServlet.java
+++ src/main/java/solvice/khd/jsptest/HelloServlet.java
@@ -12,16 +12,27 @@ public class HelloServlet extends HttpServlet {
         message = "Hello World!";
     }
 
+    //<editor-fold desc="Description 접기">
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
         response.setContentType("text/html");
+        response.setContentType("text/html; charset=utf-8");
 
         // Hello
         PrintWriter out = response.getWriter();
         out.println("<html><body>");
+        out.println("<html><body>");
         out.println("추가해서 push");
+        out.println("추가해서 push");
+        out.println("save test");
         out.println("<h1>" + message + "</h1>");
         out.println("</body></html>");
+        // TODO: 2022-08-29 ??? 이게머지 ?
+        // TODO: 2022-08-29 커밋전에 체크하시오
+
+
     }
+    //</editor-fold>
 
     public void destroy() {
     }
